4326fa361c1511802423e684b1a591be7b811818
Levi <13402525+LeviSnoot@users.noreply.github.com>
2024-10-10 23:56:39 +0200

    fix conflict

--- link_tree.py
+++ link_tree.py
@@ -5,7 +5,7 @@ API_DIR = 'api'
 OUTPUT_FILE = '_data/files.yml'
 
 def generate_link_tree(api_dir, output_file):
-    files = []
+    file_list = []
 
     for root, dirs, files in os.walk(api_dir):
         for file in files:
@@ -13,10 +13,10 @@ def generate_link_tree(api_dir, output_file):
                 file_path = os.path.join(root, file)
                 relative_path = os.path.relpath(file_path, api_dir)
                 url_path = relative_path.replace(os.sep, '/')
-                files.append({'name': url_path, 'url': f'{API_DIR}/{url_path}'})
+                file_list.append({'name': url_path, 'url': f'{API_DIR}/{url_path}'})
 
     with open(output_file, 'w') as f:
-        yaml.dump(files, f)
+        yaml.dump(file_list, f)
 
 if __name__ == '__main__':
     generate_link_tree(API_DIR, OUTPUT_FILE)
