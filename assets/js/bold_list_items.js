01293b1a8123280e647f206ad2432dfe91ff5c57
LeviSnoot <LeviSnoot@users.noreply.github.com>
2024-10-13 13:53:53 +0000

    Deploying to gh-pages from @ LeviSnoot/FNFest-Content-API@79862800bb40a81c985059c701d3e8871bae3a06 🚀

--- assets/js/bold_list_items.js
+++ assets/js/bold_list_items.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
   const listItems = document.querySelectorAll("ul li");
 
   listItems.forEach(item => {
-    const text = item.textContent;
-    if (text.includes("/")) {
-      const parts = text.split("/");
-      item.innerHTML = `${parts[0]}/<strong>${parts.slice(1).join("/")}</strong>`;
-    } else {
-      item.innerHTML = `<strong>${text}</strong>`;
+    const link = item.querySelector("a");
+    if (link) {
+      const text = link.textContent;
+      if (text.includes("/")) {
+        const parts = text.split("/");
+        link.innerHTML = `${parts[0]}/<strong>${parts.slice(1).join("/")}</strong>`;
+      } else {
+        link.innerHTML = `<strong>${text}</strong>`;
+      }
     }
   });
 });
