966800107ad1314c776b76ce27021e0ffd8600e4
Levi <13402525+LeviSnoot@users.noreply.github.com>
2024-10-13 16:33:33 +0200

    cleaner dark mode implementation

--- assets/js/theme_toggle.js
+++ assets/js/theme_toggle.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const toggleSwitch = document.querySelector('#theme-toggle');
+  const toggleIcon = document.querySelector('.theme-switch label i');
   const currentTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 
   if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === "dark") {
       toggleSwitch.checked = true;
+      toggleIcon.classList.remove('fa-toggle-off');
+      toggleIcon.classList.add('fa-toggle-on');
     }
   }
 
@@ -13,9 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     if (e.target.checked) {
       document.documentElement.setAttribute('data-theme', 'dark');
       localStorage.setItem('theme', 'dark');
+      toggleIcon.classList.remove('fa-toggle-off');
+      toggleIcon.classList.add('fa-toggle-on');
     } else {
       document.documentElement.setAttribute('data-theme', 'light');
       localStorage.setItem('theme', 'light');
+      toggleIcon.classList.remove('fa-toggle-on');
+      toggleIcon.classList.add('fa-toggle-off');
     }
   });
 });
